d88839cc9ca9343f5bea046daba63cc9ac057b01
mbernth <michael@monovoce.dk>
2016-06-23 18:02:29 +0200

    CSCC mixins etc. added

--- mobilefirst/functions.php
+++ mobilefirst/functions.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Genesis Sample.
+ * Mobile first
  *
  * This file adds functions to the Genesis Sample Theme.
  *
- * @package Genesis Sample
- * @author  StudioPress
+ * @package Mobile first mono voce
+ * @author  mono voce aps
  * @license GPL-2.0+
- * @link    http://www.studiopress.com/
+ * @link    http://www.monovoce.com/
  */
 
 //* Start the engine
@@ -17,7 +17,7 @@ include_once( get_template_directory() . '/lib/init.php' );
 include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
 
 //* Set Localization (do not remove)
-load_child_theme_textdomain( 'genesis-sample', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/languages', 'genesis-sample' ) );
+load_child_theme_textdomain( 'mobile-first', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/languages', 'mobile-first' ) );
 
 //* Add Image upload and Color select to WordPress Theme Customizer
 require_once( get_stylesheet_directory() . '/lib/customize.php' );
@@ -26,23 +26,23 @@ require_once( get_stylesheet_directory() . '/lib/customize.php' );
 include_once( get_stylesheet_directory() . '/lib/output.php' );
 
 //* Child theme (do not remove)
-define( 'CHILD_THEME_NAME', 'Genesis Sample' );
-define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
-define( 'CHILD_THEME_VERSION', '2.2.4' );
+define( 'CHILD_THEME_NAME', 'Mobile first' );
+define( 'CHILD_THEME_URL', 'http://www.monovoce.com/' );
+define( 'CHILD_THEME_VERSION', '1.0.0' );
 
 //* Enqueue Scripts and Styles
 add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
 function genesis_sample_enqueue_scripts_styles() {
 
-	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'mobile-first-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
 	wp_enqueue_style( 'dashicons' );
 
-	wp_enqueue_script( 'genesis-sample-responsive-menu', get_stylesheet_directory_uri() . '/js/responsive-menu.js', array( 'jquery' ), '1.0.0', true );
+	wp_enqueue_script( 'mobile-first-responsive-menu', get_stylesheet_directory_uri() . '/js/responsive-menu.js', array( 'jquery' ), '1.0.0', true );
 	$output = array(
-		'mainMenu' => __( 'Menu', 'genesis-sample' ),
-		'subMenu'  => __( 'Menu', 'genesis-sample' ),
+		'mainMenu' => __( 'Menu', 'mobile-first' ),
+		'subMenu'  => __( 'Menu', 'mobile-first' ),
 	);
-	wp_localize_script( 'genesis-sample-responsive-menu', 'genesisSampleL10n', $output );
+	wp_localize_script( 'mobile-first-responsive-menu', 'genesisSampleL10n', $output );
 
 }
 
@@ -54,6 +54,36 @@ add_theme_support( 'genesis-accessibility', array( '404-page', 'drop-down-menu',
 
 //* Add viewport meta tag for mobile browsers
 add_theme_support( 'genesis-responsive-viewport' );
+
+//* Add custom meta tag for mobile browsers
+add_action( 'genesis_meta', 'mono_viewport_meta_tag' );
+function mono_viewport_meta_tag() {
+	echo '<meta name="HandheldFriendly" content="True">';
+	echo '<meta name="MobileOptimized" content="320">';
+}
+// Change favicon location and add touch icons
+add_filter( 'genesis_pre_load_favicon', 'mono_favicon_filter' );
+function mono_favicon_filter( $favicon ) {
+	echo '<link rel="shortcut icon" href="'.get_bloginfo( 'stylesheet_directory' ).'/images/favicon.ico" type="image/x-icon" />';
+	echo '<link rel="apple-touch-icon" sizes="57x57" href="'.get_bloginfo( 'stylesheet_directory' ).'/images/apple-touch-icon-57x57.png">';
+	echo '<link rel="apple-touch-icon" sizes="60x60" href="'.get_bloginfo( 'stylesheet_directory' ).'/images/apple-touch-icon-60x60.png">';
+	echo '<link rel="apple-touch-icon" sizes="72x72" href="'.get_bloginfo( 'stylesheet_directory' ).'/images/apple-touch-icon-72x72.png">';
+	echo '<link rel="apple-touch-icon" sizes="76x76" href="'.get_bloginfo( 'stylesheet_directory' ).'/images/apple-touch-icon-76x76.png">';
+	echo '<link rel="apple-touch-icon" sizes="114x114" href="'.get_bloginfo( 'stylesheet_directory' ).'/images/apple-touch-icon-114x114.png">';
+	echo '<link rel="apple-touch-icon" sizes="120x120" href="'.get_bloginfo( 'stylesheet_directory' ).'/images/apple-touch-icon-120x120.png">';
+	echo '<link rel="apple-touch-icon" sizes="144x144" href="'.get_bloginfo( 'stylesheet_directory' ).'/images/apple-touch-icon-144x144.png">';
+	echo '<link rel="apple-touch-icon" sizes="152x152" href="'.get_bloginfo( 'stylesheet_directory' ).'/images/apple-touch-icon-152x152.png">';
+	echo '<link rel="apple-touch-icon" sizes="180x180" href="'.get_bloginfo( 'stylesheet_directory' ).'/images/apple-touch-icon-180x180.png">';
+	echo '<link rel="icon" type="image/png" href="'.get_bloginfo( 'stylesheet_directory' ).'/images/favicon-16x16.png" sizes="16x16">';
+	echo '<link rel="icon" type="image/png" href="'.get_bloginfo( 'stylesheet_directory' ).'/images/favicon-32x32.png" sizes="32x32">';
+	echo '<link rel="icon" type="image/png" href="'.get_bloginfo( 'stylesheet_directory' ).'/images/favicon-96x96.png" sizes="96x96">';
+	echo '<link rel="icon" type="image/png" href="'.get_bloginfo( 'stylesheet_directory' ).'/images/android-chrome-192x192.png" sizes="192x192">';
+	echo '<meta name="msapplication-square70x70logo" content="'.get_bloginfo( 'stylesheet_directory' ).'/images//smalltile.png" />';
+	echo '<meta name="msapplication-square150x150logo" content="'.get_bloginfo( 'stylesheet_directory' ).'/images//mediumtile.png" />';
+	echo '<meta name="msapplication-wide310x150logo" content="'.get_bloginfo( 'stylesheet_directory' ).'/images//widetile.png" />';
+	echo '<meta name="msapplication-square310x310logo" content="'.get_bloginfo( 'stylesheet_directory' ).'/images//largetile.png" />';
+
+}
 
 //* Add support for custom header
 add_theme_support( 'custom-header', array(
@@ -77,7 +107,7 @@ add_theme_support( 'genesis-footer-widgets', 3 );
 add_image_size( 'featured-image', 720, 400, TRUE );
 
 //* Rename primary and secondary navigation menus
-add_theme_support( 'genesis-menus' , array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
+add_theme_support( 'genesis-menus' , array( 'primary' => __( 'After Header Menu', 'mobile-first' ), 'secondary' => __( 'Footer Menu', 'mobile-first' ) ) );
 
 //* Reposition the secondary navigation menu
 remove_action( 'genesis_after_header', 'genesis_do_subnav' );
