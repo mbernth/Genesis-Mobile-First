a74417d298246ad0142770115bf55c8c9601ee8d
Michael Bernth <michael@monovoce.dk>
2016-07-18 21:39:53 +0200

    Accordion to tabs added

--- mobilefirst/page_full_width_tabs.php
+++ mobilefirst/page_full_width_tabs.php
@@ -17,9 +17,18 @@ include_once( get_stylesheet_directory() . '/lib/svg_icons.php' );
 //* Enqueue Scripts and Styles
 add_action( 'wp_enqueue_scripts', 'mobile_first_fullwidth_tabs' );
 function mobile_first_fullwidth_tabs() {
-
-	wp_enqueue_script( 'mobile-first-fullwidth-tabs', get_stylesheet_directory_uri() . '/js/cbpFWTabs.js', array( 'jquery' ), '1.0.0', true );
-	wp_enqueue_script( 'fullwidth-tabs_init', get_stylesheet_directory_uri() . '/js/tabsinit.js', array( 'jquery' ), '1.0.0', true );
+	$tabs = get_field( 'tabs' );
+	$accordion = get_field ( 'accordion_to_tabs' );
+	
+	if($tabs){
+		wp_enqueue_script( 'mobile-first-fullwidth-tabs', get_stylesheet_directory_uri() . '/js/cbpFWTabs.js', array( 'jquery' ), '1.0.0', true );
+		wp_enqueue_script( 'fullwidth-tabs_init', get_stylesheet_directory_uri() . '/js/tabsinit.js', array( 'jquery' ), '1.0.0', true );
+	}
+	
+	if($accordion){
+		wp_enqueue_script( 'accordion_tabs_jquery', get_stylesheet_directory_uri() . '/js/jquery.min.js', array( 'jquery' ), '1.0.0', true );
+		wp_enqueue_script( 'accordion_tabs', get_stylesheet_directory_uri() . '/js/accordiontabs.js', array( 'jquery' ), '1.0.0', true );
+	}
 
 }
 
@@ -61,6 +70,33 @@ function mono_fullwidth_tabs() {
 	echo '</div>';
 	}
 	
+}
+
+//* Render accordion to tabs
+add_action( 'genesis_after_entry', 'mono_accordion_tabs', 15 );
+function mono_accordion_tabs() {
+	$rows = get_field( 'accordion_to_tabs' );
+	
+	if ( is_single() || is_page ) {
+		
+		if($rows) {
+			echo '<article class="gridcontainer"><div class="wrap"><section class="coll1">';
+			echo '<ul id="nav">';
+			foreach($rows as $row) {
+				echo '<li>
+						<a href="#acctab">
+						<svg class="nine-icon nine-icon-plus"><use xlink:href="#nine-icon-plus"></use></svg>
+						<svg class="nine-icon nine-icon-minus"><use xlink:href="#nine-icon-minus"></use></svg>
+						' . $row['label']. '</a>';
+				echo '<section>';
+					echo '' . $row['content']. '';
+				echo '</section></li>';
+			$loopCounter ++;
+			}
+			echo '</ul>';
+			echo '</section></div></article>';
+		}
+	}
 }
 
 //* Run the Genesis loop
